66b0926b2adca8eb64af1d811ef63a7eb9902199
Vansh Jain <165574262+VANSHJAIN-exe@users.noreply.github.com>
2025-06-01 18:58:25 +0530

    Update ImageGenerator.tsx

--- src/components/ImageGenerator.tsx
+++ src/components/ImageGenerator.tsx
@@ -44,7 +44,7 @@ export const ImageGenerator: React.FC<ImageGeneratorProps> = ({
     'football-action': 'https://s.france24.com/media/display/451ed2b8-eed6-11ea-afdd-005056bf87d6/w:1280/p:16x9/messi-1805.jpg',
     // Tech (renamed)
     'elon-musk': '/images-all/elon musk.webp',
-    'mark-zuckerberg': '/images-all/mark zuckerberg..webp',
+    'mark-zuckerberg': '/images-all/mark zuckerberg.webp',
     'steve-jobs': '/images-all/young-steve-jobs-1_0.jpg',
     // Basketball
     'lebron-james': '/images-all/lebron james.webp',
